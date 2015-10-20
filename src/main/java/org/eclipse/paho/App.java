d0985773829e961e57e6ef0b294af2b775c6b60d
James Sutton <james.sutton@uk.ibm.com>
2015-10-20 10:07:41 +0100

    Adding EPL, EDL and Copyright statement

--- src/main/java/org/eclipse/paho/App.java
+++ src/main/java/org/eclipse/paho/App.java
@@ -1,3 +1,19 @@
+/*******************************************************************************
+ * Copyright (c) 1999, 2015 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Author:
+ *    - Originally taken from https://www.eclipse.org/paho/clients/java/
+ *    - James Sutton - Packaged Example and added TLS.
+ */
 package org.eclipse.paho;
 
 import java.io.InputStream;
